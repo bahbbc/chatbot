9b71d109fb03dc556d99b27327b567fb794e0383
Barbara Barbosa <barbarab.claudino@gmail.com>
2018-06-02 19:01:45 -0300

    Begin seq2seq

--- chatbot.py
+++ chatbot.py
@@ -82,4 +82,74 @@ answerwords2int = {}
 word_number = 0
 for word, count in word2count.items():
     if count >= threshold:
-        answerwords2int[word] = word_number
+        questionwords2int[word] = word_number
+        word_number += 1
+
+tokens = ['<PAD>', '<EOS>', '<OUT>', '<SOS>']
+
+for token in tokens:
+    questionwords2int[token] = len(questionwords2int) + 1
+
+for token in tokens:
+    answerwords2int[token] = len(answerwords2int) + 1
+
+#create the inverse dictionary of the answerswords2int dicitionary
+
+answersints2word = {w_i: w for w, w_i in answerwords2int.items() }
+
+# add the EOS token to the end of every answer
+
+for i in range(len(clean_answers)):
+    clean_answers[i] += ' <EOS>'
+
+# Tranlating all the questions and the answers into integers
+# and replacing all the words that were filtered out by <OUT>
+
+questions_into_int = []
+for question in clean_questions:
+    ints = []
+    for word in question.split():
+        if word not in questionwords2int:
+            ints.append(questionwords2int['<OUT>'])
+        else:
+            ints.append(questionwords2int[word])
+    questions_into_int.append(ints)
+
+answers_into_int = []
+for answer in clean_answers:
+    ints = []
+    for word in answer.split():
+        if word not in answerwords2int:
+            ints.append(answerwords2int['<OUT>'])
+        else:
+            ints.append(answerwords2int[word])
+        answers_into_int.append(ints)
+
+# sorting questions and answers by the lenght of the questions
+
+sorted_clean_questions = []
+sorted_clean_answers = []
+max_size_words = 25
+
+for length in range(1, max_size_words + 1):
+    for i in enumerate(questions_into_int):
+        if len(i[1]) == length:
+            sorted_clean_questions.append(questions_into_int[i[0]])
+            sorted_clean_answers.append(answers_into_int[i[0]])
+
+
+#### Seq2seq model ####
+
+def model_inputs():
+    inputs = tf.placeholder(tf.int32, [None, None], name='input')
+    targets = tf.placeholder(tf.int32, [None, None], name='target')
+    lr = tf.placeholder(tf.float32, name='learning_rate')
+    keep_prob = tf.placeholder(tf.float32, name='keep_prob')
+    return inputs, targets, lr, keep_prob
+
+
+def preprocess_targets(targets, word2int, batch_size):
+    left_side = tf.fill([batch_size, 1], word2int['<SOS>'])
+    right_side = tf.strided_slice(targets, [0,0], [batch_size, -1], [1,1])
+    preprocessed_targets = tf.concat([left_side, right_side], 1)
+    return preprocessed_targets 
